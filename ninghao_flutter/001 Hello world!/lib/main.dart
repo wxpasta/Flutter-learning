df7aaecca57dfbeff3971cffb84d1279447b5034
巴糖 <380411544@qq.com>
2024-06-26 11:15:01 +0800

    001
    项目升级

--- ninghao_flutter/001 Hello world!/lib/main.dart
+++ ninghao_flutter/001 Hello world!/lib/main.dart
@@ -9,17 +9,15 @@ void main() => runApp(Hello());
 // 自定义小部件（Widget）
 class Hello extends StatelessWidget {
   @override
-    Widget build(BuildContext context) {
-      return Center(
-        child: Text(
-          'Hello Wolrd',
-          textDirection: TextDirection.ltr,// 从左向右阅读
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Hello Word',
+          textDirection: TextDirection.ltr, // 从左向右阅读
           style: TextStyle(
             fontSize: 40.0,
             fontWeight: FontWeight.bold,
             color: Colors.white,
-          )
-        ),
-      );
-    }
+          )),
+    );
+  }
 }
